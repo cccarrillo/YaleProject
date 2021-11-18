418d06e108e046e6ded3cdaed5cf9489848e9da9
Carra Carrillo <carra.c.carrillo@erdc.dren.mil>
2021-11-18 11:48:04 -0600

    changed the 5 consecutive conditional statement to take out repeats

--- src/Duration_Difference.py
+++ src/Duration_Difference.py
@@ -112,14 +112,12 @@ def drawdown_list(elev_data, start_date, end_date):
                 date2 = elev_data.index[start_index+index]
                 OutputList.append([date1,date2,len(list),list[0],list[len(list)-1], percent_difference(list[0],list[len(list)-1])])
             list = []
-        elif (len(list) >= 10)::
-            date1 = elev_data.index[start_index+index-len(list)+1]
-            date2 = elev_data.index[start_index+index]
+        elif (len(list) >= 10):
             if list[len(list)-1] - list[len(list)-5] == 0:
-                OutputList.append([date1,date2,len(list),list[0],list[len(list)-1], percent_difference(list[0],list[len(list)-1])])
-            else:
-                
-            list = []
+                date1 = elev_data.index[start_index+index-len(list)+1]
+                date2 = elev_data.index[start_index+index]
+                OutputList.append([date1,date2,len(list),list[0],list[len(list)-1], percent_difference(list[0],list[len(list)-1])])             
+                list = []
                 
                 
             
