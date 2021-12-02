04a7aec103544e0513cf4ed37cb2b8876e09abe8
Carra Carrillo <carra.c.carrillo@erdc.dren.mil>
2021-12-02 11:04:35 -0600

    added ReadElevationData functions

--- src/Duration_Difference.py
+++ src/Duration_Difference.py
@@ -6,3 +6,37 @@ Created on Thu Nov  4 09:15:13 2021
 @author: rdel1cmc
 """
 
+#write a script that calculates the length of drawdown of at least 5 days within a season or year of each lake. for the drawdown length, i need to calculate the %difference over that time period. Also need to output the start date and end date of the drawdown period.
+#the way we are going to handle decimal points is that we will round to the nearest 0.5 ft. So the tenth place will either be 0 or 5.
+
+
+#read in the .csv file
+
+import pandas as pd
+import os
+import numpy as np
+
+
+
+def readfilename(filename):
+    return pd.read_csv(filename)
+
+def readCSVfile(filename, index):
+    return filename.iloc[index,0] #pulling out a row and a column 
+
+def getstartdate(filename,index):
+    return filename.iloc[index,1]
+
+def getenddate(filename,index):
+    return filename.iloc[index,2]
+
+def filedimensions(filename):
+    return filename.shape[0]
+
+
+
+def ReadElevationData(filename):
+    return pd.read_csv(filename, index_col ="Date")
+
+
+
