fca8adb9b50deb2750277b0f54b06e8937021525
Carra Carrillo <carra.c.carrillo@erdc.dren.mil>
2022-05-09 12:35:00 -0500

    moved primary code to src/ foldre and all the csv files to their own folder

--- src/Duration_Difference.py
+++ src/Duration_Difference.py
@@ -8,10 +8,26 @@ Created on Thu Nov  4 09:15:13 2021
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 def ReadElevationData(filename):
     """This function will read the csv input file (expected format "date, elevation") and index it based on the date column."""
     return pd.read_csv(filename)
+
+def readCSVfile(filename, index):
+    return filename.iloc[index,0]
+
+def filedimensions(filename):
+    return filename.shape[0]
+
+def GetOnlyFilename(filename):
+    return os.path.basename(filename).split(".",1)[0]
+
+def getstartdate(filename, index):
+    return filename.iloc[index,1]
+
+def getenddate(filename, index):
+    return filename.iloc[index,2]
 
 def truncate_dataframe(dataframe, startdate, enddate):
     '''This function will take in the dataframe, start date, and end date and truncate the data along those values'''
@@ -24,14 +40,15 @@ def elevation_difference(dataframe):
 
 def find_local_max_min(dataframe):
     dataframe['Max/Min'] = dataframe['Diff']+0.0000001 * dataframe['Diff'].shift(-1) < 0
+    #dataframe['Max/Min'].iloc[-1] = True
     return
 
 def get_local_max_min_points(dataframe):
     return dataframe[dataframe['Max/Min'] == True]
 
 def find_local_max(dataframe):
-    dataframe['SecondDiff'].iloc[0] = 0.0
-    dataframe['SecondDiff'].iloc[-1] = 0.0
+    #dataframe['SecondDiff'].iloc[0] = -1.0
+    #dataframe['SecondDiff'].iloc[-1] = -1.0
     return dataframe[dataframe['SecondDiff'] < 0.0]
 
 def second_Derivative(dataframe):
@@ -43,28 +60,96 @@ def rounding(dataframe, value):
     dataframe['Elevation (ft)'] = dataframe['Elevation (ft)'].apply(lambda x:round(x*value)/value)
     return
     
-test = ReadElevationData('/Users/rdel1cmc/Desktop/rdel1cmc/Desktop/Carra_ACE-IT_computer/wetlands_and_coastal/todd/bureau_of_reclamation/FY21_Info/Yale_Project/YaleProject/Data/Negative_Elevation_csv/Platoro_Reservoir_elevation_daily.csv')
-test['Date']=pd.to_datetime(test['Date'])
-test.sort_values(by='Date',inplace=True)
+def evaluation(dataframe, trundataframe, value):
+    list = []
+    output = []
+    dataframe['Drawdown'] = False
+    indexes = []
+    toremove = []
+    for index, row in trundataframe.iterrows():
+        indexes.append(index)
+    for i in range(len(indexes)-1):
+        if (indexes[i+1] - indexes[i] < 5):
+            if (dataframe['Elevation (ft)'].loc[indexes[i+1]] - dataframe['Elevation (ft)'].loc[indexes[i]] < 0):
+                toremove.append(indexes[i+1])
+    for j in range(len(toremove)):
+        for i in range(len(indexes)-1):
+            if (indexes[i+1] - indexes[i] < 5):
+                if (dataframe['Elevation (ft)'].loc[indexes[i+1]] - dataframe['Elevation (ft)'].loc[indexes[i]] < 0):
+                    if (indexes[i+1] == toremove[j]):
+                        indexes.remove(toremove[j])
+                        break
+    for i in range(len(indexes)-1):
+        if (indexes[i+1] - indexes[i] >= 5):
+            if (dataframe['Elevation (ft)'].loc[indexes[i]] - dataframe['Elevation (ft)'].loc[indexes[i]+5] >= 1/value):
+                start_date = dataframe['Date'].loc[indexes[i]]
+                start_elevation = dataframe['Elevation (ft)'].loc[indexes[i]]
+                for j in range(indexes[i+1]-indexes[i]):
+                    dataframe['Drawdown'].loc[indexes[i]+j] = True
+                    end_date = dataframe['Date'].loc[indexes[i]+j]
+                    end_elevation = dataframe['Elevation (ft)'].loc[indexes[i]+j]
+                duration = end_date - start_date
+                duration = duration.total_seconds()/60/60/24
+                percent_diff = (start_elevation - end_elevation)/(start_elevation) * 100
+                rate_of_change = (end_elevation - start_elevation) / duration
+                start_date = start_date.strftime('%Y-%m-%d %X')
+                end_date = end_date.strftime('%Y-%m-%d %X')
+                list.append([start_date, end_date, duration, start_elevation, end_elevation, percent_diff, rate_of_change])
+    return list
+    #return dataframe[dataframe['Drawdown'] == True]
+    
+def writeCSVFile(filename, listoflist):
+    out_file = open(filename, "w")
+    out_file.write("Start Date, End Date, Duration, Start Elevation, End Elevation, Percent Difference, Rate of Change\n")
+    for i in range(len(listoflist[0])):
+        out_file.write(str(listoflist[i][0]) + "," + str(listoflist[i][1]) + "," + str(listoflist[i][2]) + "," + str(listoflist[i][3]) + "," + str(listoflist[i][4]) + "," + str(listoflist[i][5]) + "," + str(listoflist[i][6]) + "\n")
+    out_file.close()
+    
+    
+    
+pathname = "/Users/rdel1cmc/Desktop/rdel1cmc/Desktop/Carra_ACE-IT_computer/wetlands_and_coastal/todd/bureau_of_reclamation/FY21_Info/Yale_Project/YaleProject/Data/"
+filename = "Metadata_File_for_runs.csv"
+readmetadatafile = ReadElevationData(pathname + filename)
+file_dimensions = filedimensions(readmetadatafile)
+
+for i in range(file_dimensions):
+    print("The file name is: {}".format(readCSVfile(readmetadatafile,i)))
+    test = ReadElevationData(pathname + readCSVfile(readmetadatafile, i))
+    test['Date']=pd.to_datetime(test['Date'])
+    test.sort_values(by='Date',inplace=True)
+    
+    print("test: {}".format(test))
+    
+    start_date = getstartdate(readmetadatafile, i)
+    print('Start Date: {}'.format(start_date))
+    end_date = getenddate(readmetadatafile,i)
+    print("End Date: {}".format(end_date))
  
-truncated_data = truncate_dataframe(test, "5/01/19", "6/01/19")
+    truncated_data = truncate_dataframe(test, start_date, end_date)
+    print("truncated data: {}".format(truncated_data))
+    rounding_value = 2
+    rounding(truncated_data,rounding_value)
 
-rounding(truncated_data,2)
+    elevation_difference(truncated_data)
+#truncated_data.plot(x='Date', y='Diff')
+#truncated_data.plot(x='Date', y='Elevation (ft)')
+#plt.show()
 
-elevation_difference(truncated_data)
-truncated_data.plot(x='Date', y='Diff')
-truncated_data.plot(x='Date', y='Elevation (ft)')
-plt.show()
+    second_Derivative(truncated_data)
+    find_local_max_min(truncated_data)
 
-second_Derivative(truncated_data)
-find_local_max_min(truncated_data)
+    temp = get_local_max_min_points(truncated_data)
 
-print(truncated_data)
+    temp = find_local_max(temp)
+
+    listoflist = evaluation(truncated_data, temp, rounding_value)
 
 
-temp = get_local_max_min_points(truncated_data)
-temp = find_local_max(temp)
+    output = writeCSVFile(GetOnlyFilename(readCSVfile(readmetadatafile,i)) + "_Duration_1FT_" + ".csv", listoflist)
+
+#print(drawdown_events)
 
 ax = truncated_data.plot(x='Date', y='Elevation (ft)')
 temp.plot.scatter(x='Date', y='Elevation (ft)', ax = ax, c='Red')
 plt.show()
+
