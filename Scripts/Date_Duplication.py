0b849816fe6cabac7df27c1b43e43989e45d8493
Carrillo <1393071318121002@mil>
2022-11-08 10:27:37 -0600

    Carra creating a directory for scripts

---
+++ Scripts/Date_Duplication.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  8 10:20:11 2022
+
+@author: RDEL1CMC
+"""
+
+##This script is going to look through csv file, compare dates, and if dates are duplicated, then 
+##the duplicated date is going to be removed from the dataset.
